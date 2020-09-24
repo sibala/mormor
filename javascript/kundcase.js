451c4cbf4246303ba6b48d3ca1f5f27db0c91206
Oscar Lundberg <oscarlundberg@Oscars-MacBook-Pro-2.local>
2020-09-24 15:46:28 +0200

    rutnät färdigt

--- javascript/kundcase.js
+++ javascript/kundcase.js
@@ -1,71 +1,71 @@
 function addRedneckMedia() {
-    document.getElementById("text-RedneckMedia").innerHTML = "<h2>Redneck Media</h2>";
+    document.getElementById("text-RedneckMedia").innerHTML = "<h3>Redneck Media</h3>";
 }
 function removeRedneckMedia() {
     document.getElementById("text-RedneckMedia").innerHTML = "";
 }
 function addAssemblyLtd() {
-    document.getElementById("text-AssemblyLtd").innerHTML = "<h2>Assembly LTD</h2>";
+    document.getElementById("text-AssemblyLtd").innerHTML = "<h3>Assembly LTD</h3>";
 }
 function removeAssemblyLtd() {
     document.getElementById("text-AssemblyLtd").innerHTML = "";
 }
 function addPhaedis() {
-    document.getElementById("text-Phaedis").innerHTML = "<h2>Phaedis</h2>";
+    document.getElementById("text-Phaedis").innerHTML = "<h3>Phaedis</h3>";
 }
 function removePhaedis() {
     document.getElementById("text-Phaedis").innerHTML = "";
 }
 function addCaava() {
-    document.getElementById("text-Caava").innerHTML = "<h2>Caava Design</h2>";
+    document.getElementById("text-Caava").innerHTML = "<h3>Caava Design</h3>";
 }
 function removeCaava() {
     document.getElementById("text-Caava").innerHTML = "";
 }
 function addAtHome() {
-    document.getElementById("text-AtHome").innerHTML = "<h2>At Home</h2>";
+    document.getElementById("text-AtHome").innerHTML = "<h3>At Home</h3>";
 }
 function removeAtHome() {
     document.getElementById("text-AtHome").innerHTML = "";
 }
 function addLittleVillageLapland() {
-    document.getElementById("text-LittleVillageLapland").innerHTML = "<h2>Little Village Lapland</h2>";
+    document.getElementById("text-LittleVillageLapland").innerHTML = "<h3>Little Village Lapland</h3>";
 }
 function removeLittleVillageLapland() {
     document.getElementById("text-LittleVillageLapland").innerHTML = "";
 }
 function addDesignerTrek() {
-    document.getElementById("text-DesignerTrek").innerHTML = "<h2>Designer Trek</h2>";
+    document.getElementById("text-DesignerTrek").innerHTML = "<h3>Designer Trek</h3>";
 }
 function removeDesignerTrek() {
     document.getElementById("text-DesignerTrek").innerHTML = "";
 }
 function addThomsonSafaris() {
-    document.getElementById("text-ThomsonSafaris").innerHTML = "<h2>Thomson Safaris</h2>";
+    document.getElementById("text-ThomsonSafaris").innerHTML = "<h3>Thomson Safaris</h3>";
 }
 function removeThomsonSafaris() {
     document.getElementById("text-ThomsonSafaris").innerHTML = "";
 }
 function addColemanSmith() {
-    document.getElementById("text-ColemanSmith").innerHTML = "<h2>Coleman Smith</h2>";
+    document.getElementById("text-ColemanSmith").innerHTML = "<h3>Coleman Smith</h3>";
 }
 function removeColemanSmith() {
     document.getElementById("text-ColemanSmith").innerHTML = "";
 }
 function addGlobekit() {
-    document.getElementById("text-Globekit").innerHTML = "<h2>Globekit</h2>";
+    document.getElementById("text-Globekit").innerHTML = "<h3>Globekit</h3>";
 }
 function removeGlobekit() {
     document.getElementById("text-Globekit").innerHTML = "";
 }
 function addFordLegend() {
-    document.getElementById("text-FordLegend").innerHTML = "<h2>Ford Legend</h2>";
+    document.getElementById("text-FordLegend").innerHTML = "<h3>Ford Legend</h3>";
 }
 function removeFordLegend() {
     document.getElementById("text-FordLegend").innerHTML = "";
 }
 function addFarmLeague() {
-    document.getElementById("text-FarmLeague").innerHTML = "<h2>Farm League</h2>";
+    document.getElementById("text-FarmLeague").innerHTML = "<h3>Farm League</h3>";
 }
 function removeFarmLeague() {
     document.getElementById("text-FarmLeague").innerHTML = "";
