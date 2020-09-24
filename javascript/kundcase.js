be0148f870c6acf5965ab47f3f96a51c1d01d791
Oscar Lundberg <oscarlundberg@Oscars-MacBook-Pro-2.local>
2020-09-24 10:16:14 +0200

    testa mouseovereffekt

---
+++ javascript/kundcase.js
@@ -0,0 +1,72 @@
+function addRedneckMedia() {
+    document.getElementById("text-RedneckMedia").innerHTML = "<h2>Redneck Media</h2>";
+}
+function removeRedneckMedia() {
+    document.getElementById("text-RedneckMedia").innerHTML = "";
+}
+function addAssemblyLtd() {
+    document.getElementById("text-AssemblyLtd").innerHTML = "<h2>Assembly LTD</h2>";
+}
+function removeAssemblyLtd() {
+    document.getElementById("text-AssemblyLtd").innerHTML = "";
+}
+function addPhaedis() {
+    document.getElementById("text-Phaedis").innerHTML = "<h2>Phaedis</h2>";
+}
+function removePhaedis() {
+    document.getElementById("text-Phaedis").innerHTML = "";
+}
+function addCaava() {
+    document.getElementById("text-Caava").innerHTML = "<h2>Caava Design</h2>";
+}
+function removeCaava() {
+    document.getElementById("text-Caava").innerHTML = "";
+}
+function addAtHome() {
+    document.getElementById("text-AtHome").innerHTML = "<h2>At Home</h2>";
+}
+function removeAtHome() {
+    document.getElementById("text-AtHome").innerHTML = "";
+}
+function addLittleVillageLapland() {
+    document.getElementById("text-LittleVillageLapland").innerHTML = "<h2>Little Village Lapland</h2>";
+}
+function removeLittleVillageLapland() {
+    document.getElementById("text-LittleVillageLapland").innerHTML = "";
+}
+function addDesignerTrek() {
+    document.getElementById("text-DesignerTrek").innerHTML = "<h2>Designer Trek</h2>";
+}
+function removeDesignerTrek() {
+    document.getElementById("text-DesignerTrek").innerHTML = "";
+}
+function addThomsonSafaris() {
+    document.getElementById("text-ThomsonSafaris").innerHTML = "<h2>Thomson Safaris</h2>";
+}
+function removeThomsonSafaris() {
+    document.getElementById("text-ThomsonSafaris").innerHTML = "";
+}
+function addColemanSmith() {
+    document.getElementById("text-ColemanSmith").innerHTML = "<h2>Coleman Smith</h2>";
+}
+function removeColemanSmith() {
+    document.getElementById("text-ColemanSmith").innerHTML = "";
+}
+function addGlobekit() {
+    document.getElementById("text-Globekit").innerHTML = "<h2>Globekit</h2>";
+}
+function removeGlobekit() {
+    document.getElementById("text-Globekit").innerHTML = "";
+}
+function addFordLegend() {
+    document.getElementById("text-FordLegend").innerHTML = "<h2>Ford Legend</h2>";
+}
+function removeFordLegend() {
+    document.getElementById("text-FordLegend").innerHTML = "";
+}
+function addFarmLeague() {
+    document.getElementById("text-FarmLeague").innerHTML = "<h2>Farm League</h2>";
+}
+function removeFarmLeague() {
+    document.getElementById("text-FarmLeague").innerHTML = "";
+}
